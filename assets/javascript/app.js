f1fdb0f8cb8e226476602e64979cc5e33862b21c
M. Neuber <mneuber55@gmail.com>
2018-09-11 10:03:45 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,20 @@
-var timeVar = 5;
+var timeVar = 6;
+var resultsSet = [];
+var correctNum = 0;
+var wrongNum = 0;
+var unanswered = 0;
+
 window.onload = function() {
     $("#start-button").on("click", start);
+    event.preventDefault();
+    $(".fieldset-box").hide();
+    $("h5").hide();
 }
 
 function start() {
     counter();
+    $(".fieldset-box").show();
+    $("h5").show();
     intervalId = setInterval(counter, 1000);
 }
 
@@ -20,10 +30,41 @@ function counter() {
 
 function timeUp() {
     clearInterval(intervalId);
-    // Submit the form and get your scores
+    validateForm();
 }
 
-function validateForm() {
+$("#submit-button").on("click", function(event) {
+    // prevent form from submitting
+    event.preventDefault();
+    validateForm();
+})
 
-    console.log($("input[name=Q1]:checked").val());
+function validateForm() {
+    alert("Time's Up!");
+    window.scrollTo(0, 0);
+    for (i=1; i<=10; i++) {
+    resultsSet.push($("input[name=Q"+i+"]:checked").val());
+    }
+    for (i=0; i<resultsSet.length; i++) {
+        if (resultsSet[i] == "yes") {
+            correctNum++;
+        }
+        else if (resultsSet[i] == "no") {
+            wrongNum++;
+        }
+        else {
+            unanswered++;
+        }
+    }
+    var right = $("<p>").text("Number Right: " + correctNum)
+    var wrong = $("<p>").text("Number Wrong: " + wrongNum);
+    var unanswer = $("<p>").text("Number Unanswered: " + unanswered);
+    $("#timer-space").html("");
+    $("#timer-space").append(right);
+    $("#timer-space").append(wrong);
+    $("#timer-space").append(unanswer);
+    resultsSet = [];
+    correctNum = 0;
+    wrongNum = 0;
+    unanswered = 0;
 }
